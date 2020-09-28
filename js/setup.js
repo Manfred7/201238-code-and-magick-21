118004e55ab60e1c4686143b6cdb192386a34701
Manfred7 <manfred7@yandex.ru>
2020-09-28 11:19:21 +0300

    Заменил мэджик намбер на константу

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,7 @@ const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `
 const WIZARD_LASTNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const WIZARD_COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const WIZARD_EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
-
+const WIZARDS_COUNT = 4;
 const userDialog = document.querySelector(`.setup`);
 userDialog.classList.remove(`hidden`);
 
@@ -33,7 +33,7 @@ const generateWizard = function () {
 
 const createWizards = function () {
   const tmpWizards = [];
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < WIZARDS_COUNT; i++) {
     tmpWizards.push(generateWizard());
   }
   return tmpWizards;
